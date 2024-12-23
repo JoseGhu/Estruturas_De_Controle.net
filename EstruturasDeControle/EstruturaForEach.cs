7facdfd1c3d2484490a2eca57c1800616c8eae21
JoseGhu <69773420+JoseGhu@users.noreply.github.com>
2024-12-22 22:46:57 -0300

    Feito por Upload

---
+++ EstruturasDeControle/EstruturaForEach.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.EstruturasDeControle
+{
+    class EstruturaForEach
+    {
+        public static void Executar()
+        {
+            var palavra = "Opa!";
+
+            foreach(var letra in palavra)
+            {
+                Console.WriteLine(letra);
+            }
+
+            var alunos = new string[] { "Ana", "Bia", "Jose" };
+
+            foreach(string aluno in alunos)
+            {
+                Console.WriteLine(aluno);
+            }
+        }
+    }
+}
